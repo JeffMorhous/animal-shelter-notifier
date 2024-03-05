14488bb95796455a90557d76751b6de87d2a5b96
Jeff Morhous <morhous.jeff@gmail.com>
2024-03-04 22:36:29 -0500

    Silence polling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   config.active_job.verbose_enqueue_logs = true
 
   config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.silence_polling = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
